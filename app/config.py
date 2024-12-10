333aa1461b27e3ac7a71da533453dcf4a18daf5e
23tae <taehoonkim.dev@gmail.com>
2024-12-11 00:06:24 +0900

    chore: 불필요한 API 삭제

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str
     database_host: str
     database_port: int
     database_name: str
--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .api import questions, analysis, generation, share
+from .api import questions, analysis
 from .database import create_tables
 from fastapi.staticfiles import StaticFiles
 from .utils.db_loader import init_db
-from pathlib import Path
 
 
 app = FastAPI(title="SinoTale API")
@@ -19,10 +18,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-app.include_router(questions.router, prefix="/api", tags=["questions"])
-app.include_router(analysis.router, prefix="/api", tags=["analysis"])
-app.include_router(generation.router, prefix="/api", tags=["generation"])
-app.include_router(share.router, prefix="/api", tags=["share"])
+app.include_router(questions.router, prefix="/api", tags=["질문지"])
+app.include_router(analysis.router, prefix="/api", tags=["캐릭터 선정"])
 
 @app.get("/")
 def read_root():
